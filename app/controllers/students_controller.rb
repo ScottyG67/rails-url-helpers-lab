d4919d0ce1f0790e725f9d0fa99f3c05ca26fad2
github username <scott.gloyna01@gmail.com>
2021-02-04 09:41:16 -0600

    commit tree c7458b36085e3a453ab3ff4c89a1f728a05411ea

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,14 +3,39 @@ class StudentsController < ApplicationController
   
   def index
     @students = Student.all
+    #binding.pry
   end
 
   def show
+    set_student
+    #binding.pry
+    set_status
+  end
+
+
+
+  def activate
+    set_student
+    @student.active = !@student.active
+    @student.save
+    set_status
+    show
+    #binding.pry
   end
 
   private
 
+  def set_status
+    if @student.active != true
+      @status = "This student is currently inactive."
+    else 
+      @status = "This student is currently active."
+    end
+  end
+
     def set_student
       @student = Student.find(params[:id])
     end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  #  put 'student/:id/activate', to: 'students#update'
+  #  put '/student/:id/activate', to: 'students#update'
+  #  put 'students/:id/activate', to: 'students#update'
+  #  put '/students/:id/activate', to: 'students#update'
+
+  #  put 'student/:id/activate', to: 'students#activate'
+  #  put '/student/:id/activate', to: 'students#activate'
+  #  put 'students/:id/activate', to: 'students#activate'
+  #  put '/students/:id/activate', to: 'students#activate'
+
+  #  get 'student/:id/activate', to: 'students#update'
+  #  get '/student/:id/activate', to: 'students#update'
+  #  get 'students/:id/activate', to: 'students#update'
+  #  get '/students/:id/activate', to: 'students#update'
+
+  #  get 'student/:id/activate', to: 'students#activate'
+  #  get '/student/:id/activate', to: 'students#activate'
+  #  get 'students/:id/activate', to: 'students#activate'
+  #  get '/students/:id/activate', to: 'students#activate'
+
+  #  patch 'student/:id/activate', to: 'students#update'
+  #  patch '/student/:id/activate', to: 'students#update'
+  #  patch 'students/:id/activate', to: 'students#update'
+  #  patch '/students/:id/activate', to: 'students#update'
+
+  #  patch 'student/:id/activate', to: 'students#activate'
+  #  patch '/student/:id/activate', to: 'students#activate'
+  #  patch 'students/:id/activate', to: 'students#activate'
+  #  patch '/students/:id/activate', to: 'students#activate'
+
+  resources :students, only: [:index, :show, :update]
+  get 'student/:id/activate', to: 'students#activate', as: 'activate_student'
+
 end
